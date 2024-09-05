21f5137b4115f65d65f20a54b98b8b1710f2c89b
MarkusEfr <markus.efr@gmail.com>
2024-09-06 01:56:39 +0300

    fix: owner wallet

--- lib/birdpaw/utils/presale_util.ex
+++ lib/birdpaw/utils/presale_util.ex
@@ -6,17 +6,18 @@ defmodule Birdpaw.PresaleUtil do
   @eth_conversion_rate 6_000_000
   @usdt_conversion_rate 0.00042
   @usdt_decimal_factor :math.pow(10, 6) |> round()
+  @owner_wallet "0xDc484b655b157387B493DFBeDbeC4d44A248566F"
 
   def generate_qr_code({wei_amount, amount}, wallet_address, payment_method) do
     payment_uri =
       case payment_method do
         "ETH" ->
-          "ethereum:#{wallet_address}?value=#{wei_amount}"
+          "ethereum:#{@owner_wallet}?value=#{wei_amount}"
 
         "USDT" ->
           usdt_amount_in_smallest_units = round(amount * @usdt_decimal_factor)
 
-          "ethereum:0xdAC17F958D2ee523a2206206994597C13D831ec7/transfer?address=#{wallet_address}&uint256=#{usdt_amount_in_smallest_units}"
+          "ethereum:0xdAC17F958D2ee523a2206206994597C13D831ec7/transfer?address=#{@owner_wallet}&uint256=#{usdt_amount_in_smallest_units}"
 
         _ ->
           raise "Unsupported payment method"
